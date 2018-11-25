728442606062272523f031013dd66b7b25c7d3b9
alberto <albertogarciarin97@gmail.com>
2018-11-25 20:01:35 +0100

    chapuza1

--- src/main.cpp
+++ src/main.cpp
@@ -11,55 +11,101 @@
 
 
 boost::posix_time::ptime now = boost::date_time::not_a_date_time;
-boost::posix_time::ptime last = boost::date_time::not_a_date_time;
+boost::posix_time::ptime last1 = boost::date_time::not_a_date_time;
+boost::posix_time::ptime last2 = boost::date_time::not_a_date_time;
+boost::posix_time::ptime last3 = boost::date_time::not_a_date_time;
+boost::posix_time::ptime last4 = boost::date_time::not_a_date_time;
+boost::posix_time::ptime last5 = boost::date_time::not_a_date_time;
+
 
 
 int main()
 {
-    last = boost::posix_time::microsec_clock::universal_time();
+    last2 = boost::posix_time::microsec_clock::universal_time();
+    int y = 0;
+
     boost::asio::io_service io_service;
     boost::asio::serial_port serial_port(io_service, "/dev/ttyACM0");
     //boost::asio::serial_port serial_port(io_service, "/dev/tty.usbmodem931901443E");
 
     vmu931::Sensor sensor(std::move(serial_port));
+
     sensor.register_sink([](vmu931::Accelerometers accel) {
-        std::cout << "accel: x=" << accel.x << " y=" << accel.y << " z=" << accel.z <<"\n";
+      now = boost::posix_time::microsec_clock::universal_time();
+          std::cout << "accel: x=" << accel.x << " y=" << accel.y << " z=" << accel.z <<"\n";
+      std::cout <<"Time: " <<now <<" --Dif: " <<now-last1 <<std::endl;
+      last1 = now;
+/*
+      if(now-last1 >= boost::posix_time::milliseconds(100)) {
+        std::cout <<now <<std::endl;
+        std::cout <<now-last1 <<std::endl;
+          std::cout << "accel: x=" << accel.x << " y=" << accel.y << " z=" << accel.z <<"\n";
+        last1 = now;
+      }*/
     });
+
     sensor.register_sink([](vmu931::Gyroscopes gyro) {
-        std::cout << "gyro: x=" << gyro.x << " y=" << gyro.y << " z=" << gyro.z <<"\n";
+      now = boost::posix_time::microsec_clock::universal_time();
+          std::cout << "gyro: x=" << gyro.x << " y=" << gyro.y << " z=" << gyro.z <<"\n";
+      std::cout <<"Time: " <<now <<" --Dif: " <<now-last2 <<std::endl;
+      last2 = now;
+/*
+      if(now-last2 >= boost::posix_time::milliseconds(100)) {
+        std::cout <<now <<std::endl;
+        std::cout <<now-last2 <<std::endl;
+          std::cout << "gyro: x=" << gyro.x << " y=" << gyro.y << " z=" << gyro.z <<"\n";
+        last2 = now;
+      }*/
     });
+
     sensor.register_sink([](vmu931::Magnetometers magneto) {
         std::cout << "magneto: x=" << magneto.x << " y=" << magneto.y << " z=" << magneto.z <<"\n";
     });
 
-
     sensor.register_sink([](vmu931::EulerAngles euler) {
-      /*now = boost::posix_time::microsec_clock::universal_time();
-      std::cout<<now-last<<std::endl;
-      if(now-last >= boost::posix_time::seconds(1.0)) {
-        std::cout <<now <<std::endl;
+      now = boost::posix_time::microsec_clock::universal_time();
         std::cout << "euler: x=" << euler.x << " y=" << euler.y << " z=" << euler.z <<"\n";
-        last = now;
+      std::cout <<"Time: " <<now <<" --Dif: " <<now-last3 <<std::endl;
+      last3 = now;
+/*
+      if(now-last3 >= boost::posix_time::milliseconds(100)) {
+        std::cout <<now <<std::endl;
+        std::cout <<now-last3 <<std::endl;
+          std::cout << "euler: x=" << euler.x << " y=" << euler.y << " z=" << euler.z <<"\n";
+        last3 = now;
       }*/
-
-      std::cout << "euler: x=" << euler.x << " y=" << euler.y << " z=" << euler.z <<"\n";
-
-      auto start = boost::posix_time::microsec_clock::universal_time();
-      auto stop = boost::posix_time::microsec_clock::universal_time();
-      auto diff = stop - start;
-      auto to_sleep = boost::posix_time::seconds(1.0) - diff;
-      usleep(to_sleep.total_microseconds());
     });
-
 
     sensor.register_sink([](vmu931::Quaternions quat) {
+      now = boost::posix_time::microsec_clock::universal_time();
         std::cout << "quat: w=" << quat.w << " x=" << quat.x << " y=" << quat.y << " z=" << quat.z <<"\n";
+      std::cout <<"Time: " <<now <<" --Dif: " <<now-last4 <<std::endl;
+      last4 = now;
+/*
+      if(now-last4 >= boost::posix_time::milliseconds(100)) {
+        std::cout <<now <<std::endl;
+        std::cout <<now-last4 <<std::endl;
+          std::cout << "quat: w=" << quat.w << " x=" << quat.x << " y=" << quat.y << " z=" << quat.z <<"\n";
+        last4 = now;
+      }*/
     });
+
     sensor.register_sink([](vmu931::Heading h) {
       now = boost::posix_time::microsec_clock::universal_time();
-      std::cout <<now <<std::endl;
         std::cout << "heading: " << h.heading << "\n";
+      std::cout <<"Time: " <<now <<" --Dif: " <<now-last5 <<std::endl;
+      last5 = now;
+      std::cout <<std::endl;
+/*
+      if(now-last5 >= boost::posix_time::milliseconds(100)) {
+        std::cout <<now <<std::endl;
+        std::cout <<now-last5 <<std::endl;
+          std::cout << "heading: " << h.heading << "\n";
+        last5 = now;
+        std::cout <<std::endl;
+      }*/
     });
+
     sensor.register_sink([](std::string s) {
         std::cout << "message: " << s << "\n";
     });
@@ -73,27 +119,20 @@ int main()
 
     io_service.post([&sensor]() {
       sensor.set_streams({
-        //vmu931::commands::Accelerometers,
-        //vmu931::commands::Gyroscopes,
+        vmu931::commands::Accelerometers,
+        vmu931::commands::Gyroscopes,
         //vmu931::commands::Magnetometers,
         vmu931::commands::EulerAngles,
-        //vmu931::commands::Quaternions,
-        //vmu931::commands::Heading,
+        vmu931::commands::Quaternions,
+        vmu931::commands::Heading,
         //vmu931::commands::Status
       });
     });
 
     std::cout << "Start reading VMU931 sensor stream...\n";
     io_service.run();
-    /*while(true) {
-      auto start = boost::posix_time::microsec_clock::universal_time();
-      io_service.poll_one();
-      auto stop = boost::posix_time::microsec_clock::universal_time();
-      auto diff = stop - start;
-      auto to_sleep = boost::posix_time::seconds(1.0) - diff;
-      usleep(to_sleep.total_microseconds());
-    }*/
-    return 0;
+
+  return 0;
 }
 
 
